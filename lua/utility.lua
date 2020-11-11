424e8b7f94501f0cfd5b0e058256b83c1d21ea95
unknown <2592419242@qq.com>
2020-11-12 00:28:01 +0800

    Update lua/utility

--- lua/utility.lua
+++ lua/utility.lua
@@ -1,17 +1,18 @@
 local MODULE = {}
 
-function MODULE.copy(table)
-	local function copy(old)
-		if type(old) ~= "table" then
-			return old
-		end
-		local new = {}
-		for key, value in pairs(old) do
-			new[copy(key)] = copy(value)
-		end
-		return new
+local function copy(old)
+	if type(old) ~= "table" then
+		return old
 	end
 
+	local new = {}
+	for key, value in pairs(old) do
+		new[copy(key)] = copy(value)
+	end
+	return new
+end
+
+function MODULE.copy(table)
 	return copy(table)
 end
 
